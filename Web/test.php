407d7710b13889c1145ece5556b1154d361d631e
lee-app <kli4574@naver.com>
2018-11-16 23:09:35 +0900

    테스트용 페이지 추가

---
+++ Web/test.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee-app
+ * Date: 2018-11-16
+ * Time: 오후 9:25
+ */
+
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+ini_set('register_globals', 'off');
+
+
+$ip = $_POST["ip"];
+$port = $_POST["port"];
+$request_number = $_POST["request_number"];
+$subject_id = $_POST["subject_id"];
+$assignment_id = $_POST["assignment_id"];
+
+$id = $_POST["id"];
+$text = $_POST["text"];
+
+$file_name = "./files/" . $id . ".c";
+if(!file_exists("./files/")) mkdir("./files/");
+$file = fopen($file_name, "w+");
+$result = fwrite($file, $text);
+fclose($file);
+
+// file_put_contents ($dir.'/test.txt', 'Hello File');
+
+if($result) {
+	echo "success make file : " . $file_name . "</br>";
+	$command = "./files/web_sender $ip $port $request_number $id $subject_id $assignment_id";
+	$exec_result = exec($command);
+	echo $exec_result;
+}
+else echo "fail make file";
