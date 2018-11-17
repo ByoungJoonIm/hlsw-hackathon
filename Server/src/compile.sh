bae926a34002874ff7ea400488c0929aebd22ad2
BJ-Lim <fortress0427@gmail.com>
2018-11-17 12:47:19 +0900

    exe file upload

--- Server/src/compile.sh
+++ Server/src/compile.sh
@@ -4,16 +4,15 @@ len=`expr ${#1} - 2`
 name=${1:0:$len}
 
 # compile error is written to $name.rs
-rs=`gcc $name.c -o $name > "$name.rs" 2>&1`
-
+gcc $name.c -o $name > "$name.rs" 2>&1
 
 filesize=$(wc -c < "$name.rs")
 echo $filesize
 if [ $filesize -gt 0 ]
 then
-	echo "compile failed!"
+	echo "compile failed!" > test.txt
 else
-	echo "compile successed!"
+	echo "compile successed!" > test.txt
 # execution result is written to $name.rs
 	./$name > $name.rs
 fi
